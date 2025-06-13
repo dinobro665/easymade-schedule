926bb74205074cbd9cc0e5b640787510b86ca099
Oh Yoon Seok <oys110801@gmail.com>
2025-06-13 16:36:56 +0900

    fix: explicitly set publicDir to ensure redirects/admin deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mdPlugin from 'vite-plugin-markdown'
+
+const markdown = mdPlugin.default
 
 export default defineConfig({
-  plugins: [react()],
+  publicDir: 'public', // ✅ 이 줄을 추가해줘
+  plugins: [
+    react(),
+    markdown({ mode: ['html'] }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 });
+
